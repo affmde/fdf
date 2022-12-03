06121bb124318cf384cf3081ce2038a7b92972dc
affmde <affmde@LAPTOP-R2ML70MJ>
2022-12-03 15:21:04 +0200

    set-color added to fix line

--- srcs/bresenham_algo.c
+++ srcs/bresenham_algo.c
@@ -6,12 +6,21 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 16:10:28 by andrferr          #+#    #+#             */
-/*   Updated: 2022/12/03 12:49:22 by andrferr         ###   ########.fr       */
+/*   Updated: 2022/12/03 15:17:25 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../my_libft/libft.h"
 #include "../includes/fdf.h"
+
+static void	set_color(t_pos *pos0, t_pos *pos1)
+{
+	if(pos0->z || pos1->z)
+	{
+		pos0->color = 0xFF0000;
+		pos1->color = 0Xffffff;
+	}
+}
 
 static void line_params(t_pos pos_0, t_pos pos_1, t_params *params)
 {
@@ -34,6 +43,7 @@ void	bresenham_algo(t_pos pos_0, t_pos pos_1, t_img *image)
 	t_params	params;
 
 	line_params(pos_0, pos_1, &params);
+	set_color(&pos_0, &pos_1);
 	while (pos_0.y != pos_1.y || pos_0.x != pos_1.x)
 	{
 		my_put_pixel(image, pos_0.x, pos_0.y, pos_0.color);
